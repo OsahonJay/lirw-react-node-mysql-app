f309f416a67345a61099e64006799a2260446c59
OsahonJay <osahonjustine@gmail.com>
2026-07-11 12:30:11 +0100

    Fix after_install to run npm install on instance

--- backend/scripts/after_install.sh
+++ backend/scripts/after_install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -e
 
 echo "Installing backend dependencies on instance"
 cd /home/ec2-user/app/backend
-npm install --production
+npm install
 
 echo "Setting correct ownership"
 chown -R ec2-user:ec2-user /home/ec2-user/app/backend
